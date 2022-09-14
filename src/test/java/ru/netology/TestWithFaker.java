d27f8512f1889d40960a4eb161387397d1562a2c
Диана Бережная <Lacshri@yandex.ru>
2022-09-14 16:16:13 +0300

    date correction

--- src/test/java/ru/netology/TestWithFaker.java
+++ src/test/java/ru/netology/TestWithFaker.java
@@ -35,9 +35,8 @@ public class TestWithFaker {
         $(byName("name")).val(info.getName());
         $("[name='phone']").val(info.getPhone());
         $x("//span[@class='checkbox__box']").click();
-        $$("[class='button__text']").get(0).click();
-        $$(".notification__content")
-                .get(0)
+        $("[class='button__text']").click();
+        $(".notification__content")
                 .shouldBe(visible)
                 .shouldBe(appear, Duration.ofSeconds(15))
                 .should(exactText("Встреча успешно запланирована на " + deliveryDate));
@@ -45,7 +44,7 @@ public class TestWithFaker {
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id='date']").click();
         LocalDate dateDefault = LocalDate.now().plusDays(3);
-        LocalDate dateOfMeeting = LocalDate.of(2022, 9, 20);
+        LocalDate dateOfMeeting = LocalDate.now().plusDays(4);
         String dayToSearch = String.valueOf(dateOfMeeting.getDayOfMonth());
         if (dateOfMeeting.getMonthValue() > dateDefault.getMonthValue() | dateOfMeeting.getYear() > dateDefault.getYear()) {
             $(".calendar__arrow_direction_right[data-step='1']").click();
@@ -56,10 +55,9 @@ public class TestWithFaker {
                 .shouldBe(appear, Duration.ofSeconds(15))
                 .shouldBe(visible);
         $$("[class='button__text']").get(1).click();
-        $$x("//div[@class='notification__content']")
-                .get(0)
+        $x("//div[@class='notification__content']")
                 .shouldBe(appear, Duration.ofSeconds(15))
-                .shouldBe(visible)
-                .should(text("Встреча успешно запланирована на " + dayToSearch));
+                .shouldBe(visible).should(text("Встреча успешно запланирована на " + dayToSearch));
     }
 }
+
